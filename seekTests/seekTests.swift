51bbd5826cac80b4553c949d98572cadc5afecda
Amos Seah <amosseah@Amoss-MacBook-Pro.local>
2015-05-17 21:44:46 +0800

    Initial Commit

---
+++ seekTests/seekTests.swift
@@ -0,0 +1,36 @@
+//
+//  seekTests.swift
+//  seekTests
+//
+//  Created by Amos Seah on 17/5/15.
+//  Copyright (c) 2015 Amos Seah. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class seekTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
